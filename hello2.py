2bc75be2128d9ad1526cbf72633a696a9e335903
Theresa Swayne <theresa@Mini-Swemtage.local>
2017-03-07 23:32:56 -0500

    Checking that spyder works

---
+++ hello2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 23:28:51 2017
+
+@author: theresa
+"""
+# created in new version of spyder
+print('hello world')
+print('I like 6.00.2x!')
