c9c3868a231ce4dd7df3d2a81dc8bcc506081b72
Theresa Swayne <theresa@Mini-Swemtage.local>
2017-03-12 23:42:11 -0400

    implementing trinity list
    set up 2 combos and added notes on idea for using modulo 3

--- lecture02-fex1.py
+++ lecture02-fex1.py
@@ -17,24 +17,43 @@ def yieldAllCombos(items):
     # this is the original code for 1 bag
     # generate all combinations of N items
     N = len(items)
-    # print("N = "+str(N))
-    # enumerate the 2**N possible combinations
-    for i in range(2**N):
-        # print("i = "+ str(i))
-        combo = []
+    print("N = "+str(N))
+    # enumerate the 3**N possible combinations
+    for i in range(3**N):
+        combo1 = []
+        combo2 = []
+        # Vary something to get each possibility, one at a time
         for j in range(N):
-            # print("j = "+str(j))
-            # test bit jth of integer i
-            if (i >> j) % 2 == 1:
-                combo.append(items[j])
-        yield combo
+            print("i = "+ str(i),"j = "+str(j))
+            # Test condition to see where to put items
+            if i % N == 0:
+                continue
+            elif i % N == 1:
+                combo1.append(items[j])
+            elif i % N == 2:
+                combo2.append(items[j])
+        yield (combo1, combo2)
         
     # note there are 3**N possible combinations for the 2-bag problem.
     # "You can imagine this by representing the two bags 
     # as a list of "trinary" bits, 0, 1, or 2 
     # (a 0 if an item is in neither bag; 1 if it is in bag1; 2 if it is in bag2). "
+    # Try to represent as a list of length N
+    # where each element i is the location of items[i]. 
+    # Go through this list and if the element is 1 
+    # append the corresponding item to bag1 etc.
     
-    
+#    Use modulo 3
+#But instead of a bitwise binary shift you divide by 3 to some power
+# int(i/3) % 3
+
+# int(i/(3**(N-j) % 3
+#Loop j from 0 to N+1
+#Look at i/(3^(N-j)) % 3
+#
+#That gives you the location of item j
+
+
 # testing
 items = ["bat", "ball", "glove"]
 
